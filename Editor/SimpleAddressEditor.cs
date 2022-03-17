2ad3331a5e3bd54605f83f8bcb52254ed9410dcf
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-03-17 15:30:48 -0400

    Implement SimpleAddressEditor
    This editor will render arrays as ReorderableLists and all other properties using a PropertyField
    This is a UIToolkit Editor

---
+++ Editor/SimpleAddressEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace PassivePicasso.SimplyAddress
+{
+    [CustomEditor(typeof(SimpleAddress), true)]
+    public class SimpleAddressEditor : Editor
+    {
+        protected virtual bool CanUseAddress() => true;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            VisualElement root = new VisualElement();
+            var iterator = serializedObject.GetIterator().Copy();
+            var children = iterator.GetVisibleChildren().Where(child => child.name != "m_Script").ToArray();
+            foreach (var child in children)
+            {
+                if (child.isArray && child.arrayElementType != "char")
+                {
+                    var rList = new ReorderableList(child.serializedObject, child, true, true, true, true);
+
+                    void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused) => EditorGUI.PropertyField(rect, rList.serializedProperty.GetArrayElementAtIndex(index));
+                    void OnHeader(Rect rect) => GUI.Label(rect, child.displayName);
+
+                    rList.drawHeaderCallback += OnHeader;
+                    rList.drawElementCallback += OnDrawElement;
+
+                    var imguiContainer = new IMGUIContainer(() =>
+                    {
+                        serializedObject.Update();
+                        rList.DoLayoutList();
+                        serializedObject.ApplyModifiedProperties();
+                    });
+                    root.Add(imguiContainer);
+                }
+                else
+                    root.Add(new PropertyField(child));
+            }
+            return root;
+        }
+    }
+}
