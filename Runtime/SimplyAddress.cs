8a6b1ab106153524d81a005b80739f774a87c00d
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-03-17 16:52:10 -0400

    enable as BepinPlugin

---
+++ Runtime/SimplyAddress.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace PassivePicasso.SimplyAddress
+{
+    using static PassivePicasso.SimplyAddress.Constants;
+    public static class Constants
+    {
+        public const string Name = "SimplyAddress";
+        public const string GUID = "com.passivepicasso.simplyaddress";
+        public const string Version = "0.0.1";
+    }
+
+    [BepInPlugin(GUID, Name, Version)]
+    public class SimplyAddress : BaseUnityPlugin
+    {
+
+    }
+}
